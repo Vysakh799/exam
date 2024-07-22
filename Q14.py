09876ec7a59fcc436bd98484b628e92c7c6c7b51
Vysakh799 <vysakh.dev.official@gmail.com>
2024-07-22 07:58:44 +0530

    pattern

--- Q14.py
+++ Q14.py
@@ -1,3 +1,26 @@
+for i in range(1,6):
+    for j in range(i):
+        print((i+j)%2,end=" ")
+    print()
+
+
 for i in range(1,5):
     for j in range(i):
-        print()
+        if i%2==0:
+            if j%2==0:
+                print(0,end="")
+            else:
+                print(1,end="")
+        else:
+            if j%2==0:
+                print(1,end="")
+            else:
+                print(0,end="")
+    print()
+
+
+# 1 
+# 0 1 
+# 1 0 1 
+# 0 1 0 1 
+# 1 0 1 0 1   
